804a468391f7ca64e1aaaa69de5299cd2e7ec8c1
Johnathon <kd.boss@gmail.com>
2021-04-02 14:43:24 -0500

    removing compiled files

--- examples/wintest/wintest.cpp
+++ examples/wintest/wintest.cpp
@@ -1,10 +1,10 @@
 /*
     WinTest, Small Demo application that creates a user interface window.
     release build - compile with :
-		winres -i wintest.rc -o wintestrc.o 
+		winres -i wintest.rc -o wintestrc.o
         g++ wintest.cpp wintestrc.o -municode -mwindows -s -O3 -o wintest.exe
     debug build - compile with :
-		winres -i wintest.rc -o wintestrc.o 
+		winres -i wintest.rc -o wintestrc.o
         g++ wintest.cpp wintestrc.o -municode -mwindows -g -o wintest.exe
 */
 
@@ -28,8 +28,8 @@ void MyWindow::OnExit(UINT uNotifyCode, int nID, Window wndCtl)
 }
 
 void MyWindow::OnAbout(UINT uNotifyCode, int nID, Window wndCtl)
-{		
-	
+{
+
 	if(!about)
 	{
 		if(about.DoModal() == IDOK)
@@ -37,7 +37,7 @@ void MyWindow::OnAbout(UINT uNotifyCode, int nID, Window wndCtl)
 			MessageBox::Show(TEXT("OK pressed!"), TEXT("WinTest"),MessageBoxButtons::Ok, MessageBoxIcon::Information);
 		}
 	}
-	
+
 }
 
 void MyWindow::OnClose()
@@ -47,7 +47,7 @@ void MyWindow::OnClose()
 
 int MyWindow::OnCreate(LPCREATESTRUCT lpCreateStruct)
 {
-	//additional window initalization would go here. 
+	//additional window initalization would go here.
     return 0;
 }
 
@@ -55,27 +55,27 @@ void MyWindow::OnLButtonDown(UINT nFlags, const Point &pt)
 {
     	std::tstringstream str;
         str << TEXT("Clicked At: ") << pt.x << TEXT(",") << pt.y;
-        MessageBox::Show(str.str().c_str(), TEXT("WinTest"),MessageBoxButtons::Ok , MessageBoxIcon::Information);   
+        MessageBox::Show(str.str().c_str(), TEXT("WinTest"),MessageBoxButtons::Ok , MessageBoxIcon::Information);
 }
 
-int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPTSTR, int nShow)
+int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int nShow)
 {
     MyWindow win;
     win.Create(nullptr, &Window::rcDefault, TEXT("Test Window"));
     win.ShowWindow(nShow);
     win.UpdateWindow();
-	
+
     MSG msg;
-	
+
 	HACCEL hAccelTable = LoadAccelerators(HINST_THISCOMPONENT, MAKEINTRESOURCE(IDC_WINTEST));
-	
+
 	INITCOMMONCONTROLSEX icc;
 
 	// Initialise common controls.
 	icc.dwSize = sizeof(icc);
 	icc.dwICC = ICC_WIN95_CLASSES;
 	InitCommonControlsEx(&icc);
-	
+
     while (GetMessage(&msg, nullptr, 0, 0))
     {
 		if(!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
@@ -83,7 +83,7 @@ int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPTSTR, int nShow)
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
-		
+
     }
     return msg.wParam;
 }
